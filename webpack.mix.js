c8552ca13e6bd3f98f5497f33d49439828361603
Alexandr Katerinin <sasha.katerinin@gmail.com>
2022-04-08 16:38:18 +0600

    fixes 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const config = require('./webpack.config');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sourceMaps()
+    .sourceMaps(true, 'source-maps')
     .vue()
     .webpackConfig(config)
     .version()
